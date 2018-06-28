41a0dbe53f28b16eaaf5b6b0824cead3ad6fedce
drbaminh456 <dangbaminh260896@gmail.com>
2018-06-28 17:09:59 +0700

    commit

--- src/main/java/com/example/demo/TableApplication.java
+++ src/main/java/com/example/demo/TableApplication.java
@@ -2,10 +2,18 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
+@RestController
+@RequestMapping
+@EnableAutoConfiguration
 public class TableApplication {
 	@Autowired
 	private TableDao dao;
@@ -16,6 +24,7 @@ public class TableApplication {
 
 		return new ResponseEntity<>(res, HttpStatus.OK);
 	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(TableApplication.class, args);
 	}
