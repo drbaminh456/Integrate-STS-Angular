8a8582e863af7be13fab33145e5379c4d68fbfe6
drbaminh456 <dangbaminh260896@gmail.com>
2018-07-02 15:31:39 +0700

    API Login.

--- src/main/java/com/example/dal/TestDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.dal;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.model.Test;
-
-public interface TestDao extends CrudRepository<Test, Integer> {
-
-}
